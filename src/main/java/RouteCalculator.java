d83de05c029df8acc933ee4692c0e19090f6b594
burovtsov <burovcov@gmail.com>
2019-09-06 16:48:12 +0300

    added logging with log4j2; made 3 loggers

--- src/main/java/RouteCalculator.java
+++ src/main/java/RouteCalculator.java
@@ -25,7 +25,7 @@ public class RouteCalculator
         }
 
         route = getRouteWithOneConnection(from, to);
-        if((route != null) && (route.size() != 0)) { //added "&& (route.size() != 0)"
+        if((route != null) && (route.size() != 0)) {                     //added "&& (route.size() != 0)"
             List<Station> route2 = getRouteWithTwoConnections(from, to); //added to fix bug with don't checked route with two connections
             if (calculateDuration(route2) < calculateDuration(route))    //added to fix bug with don't checked route with two connections
                 return route2;                                           //added to fix bug with don't checked route with two connections
